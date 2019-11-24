062eb5cae6d1dcef6491765c056f2c00d2389e8d
josh hieb <joshwhieb@gmail.com>
2019-11-24 12:22:12 -0700

    Add key mapping and initial force applied to tracks.
    Need to overcome friction now to move.

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -3,3 +3,15 @@
 
 #include "TankTrack.h"
 
+
+void UTankTrack::SetThrottle(float Throttle) {
+	//auto Time = GetWorld()->GetTimeSeconds();
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *Name, Throttle);
+
+	// TODO clamp actual throttle value so player can't go too fast.
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+}
